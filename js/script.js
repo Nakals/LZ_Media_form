1a7d026d2941baa0804542c4966a380b38ca798e
Nakals <gavvanastya98@gmail.com>
2023-01-24 15:31:39 +0100

    edit pattern

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-function showPass() {
-    var inpute = document.getElementById("showPass_reg_label");
-    if (inpute.type === "password") {
-        inpute.type = "text";
-    } else {
-        inpute.type = "password";
-    }
-}
-
 $(document).ready(function(){
     $('.section_switch label').on('click', function(){
         $('.form_wrapper').toggleClass('hidden');
     });
     $('.showPass_wrapper label[for=showPass_reg], .showPass_wrapper label[for=showPass_login]').on('click',function(){
         var pass = $(this).closest('.password_wrapper').children('input[name=password]').attr('type');
-        console.log(pass);
         if(pass === "password"){
             $(this).closest('.password_wrapper').children('input[name=password]').attr('type', 'text');
             $(this).children('img').attr('src','image/view.svg');
