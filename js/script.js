c50e0bc25e21634665ae43a1ac174b1c84876e4c
Nakals <gavvanastya98@gmail.com>
2023-01-24 15:03:40 +0100

    add breakpoints css

--- js/script.js
+++ js/script.js
@@ -12,14 +12,14 @@ $(document).ready(function(){
         $('.form_wrapper').toggleClass('hidden');
     });
     $('.showPass_wrapper label[for=showPass_reg], .showPass_wrapper label[for=showPass_login]').on('click',function(){
-        var pass = $(this).closest('.password_wrapper').children('#password').attr('type');
+        var pass = $(this).closest('.password_wrapper').children('input[name=password]').attr('type');
         console.log(pass);
         if(pass === "password"){
-            $(this).closest('.password_wrapper').children('#password').attr('type', 'text');
+            $(this).closest('.password_wrapper').children('input[name=password]').attr('type', 'text');
             $(this).children('img').attr('src','image/view.svg');
         }
         else{
-            $(this).closest('.password_wrapper').children('#password').attr('type', 'password');
+            $(this).closest('.password_wrapper').children('input[name=password]').attr('type', 'password');
             $(this).children('img').attr('src','image/eye.svg');
         }
     });
